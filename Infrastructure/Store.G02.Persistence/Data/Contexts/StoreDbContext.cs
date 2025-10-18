29f68f3795c8dfab086ba8cf180b944aba75d917
Mai <Mai@DESKTOP-MILA5UN>
2025-10-18 06:59:14 +0400

    ..

---
+++ Infrastructure/Store.G02.Persistence/Data/Contexts/StoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Persistence.Data.Contexts
+{
+    internal class StoreDbContext : DbContext
+    {
+    }
+}
