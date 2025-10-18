67404ce4c5294da0c130ac02b8b99322d0358871
Mai <Mai@DESKTOP-MILA5UN>
2025-10-18 09:14:22 +0400

    Session02-V07

---
+++ Core/Store.Go2.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Store.G02.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        // Generate Repository
+        IGenericRepository<TKey, TEntity> GetRepository<TKey, TEntity>() where TEntity : BaseEntity<TKey>;
+
+        // Save Changes
+        Task<int> SaveChangesAsync();
+
+    }
+}
