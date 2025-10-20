37ccd33fd06f9c33a2ed7930768c99b26127f7a1
Mai <Mai@DESKTOP-MILA5UN>
2025-10-20 21:26:42 +0400

    Session03-V06

---
+++ Core/Store.G02.Services/Specifications/Products/ProductsWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,30 @@
+﻿using Store.G02.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Services.Specifications.Products
+{
+    public class ProductsWithBrandAndTypeSpecifications : BaseSpecifications<int, Product>
+    {
+        public ProductsWithBrandAndTypeSpecifications(int id) : base(P => P.Id == id)
+        {
+            ApplyIncludes();
+        }
+        public ProductsWithBrandAndTypeSpecifications() : base(null)
+        {
+            ApplyIncludes();
+        }
+
+
+        private void ApplyIncludes()
+        {
+            Includes.Add(P => P.Brand);
+            Includes.Add(P => P.Type);
+        }
+
+    }
+}
