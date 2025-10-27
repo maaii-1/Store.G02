1c5a1454287a410c299d23e34d6cb03f5fd8484d
Mai <Mai@DESKTOP-MILA5UN>
2025-10-27 08:21:32 +0400

    Session04-V06

---
+++ Core/Store.G02.Services/ApplicationServicesRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Store.G02.Services.Abstraction;
+using Store.G02.Services.Mapping.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Services
+{
+    public static class ApplicationServicesRegistration
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddAutoMapper(M => M.AddProfile(new ProductProfile(configuration)));
+
+            return services;
+        }
+
+    }
+}
---
+++ Infrastructure/Store.G02.Persistence/InfrastructureServicesRegistration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Store.G02.Domain.Contracts;
+using Store.G02.Persistence.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Persistence
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+            services.AddScoped<IDbInitializer, DbInitializer>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
