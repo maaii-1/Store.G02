c810c58d1b2a651573f71ee65ed76924680b7e90
Mai <Mai@DESKTOP-MILA5UN>
2025-10-29 10:40:38 +0400

    Session05-V04

---
+++ Infrastructure/Store.G02.Presentation/BasketsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.G02.Services.Abstraction;
+using Store.G02.Shared.Dtos.Baskets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketsController(IServiceManager _serviceManager) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetBasketById(string id)
+        {
+            var result = await _serviceManager.BasketService.GetBasketAsync(id);
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBasketById(BasketDto basketDto)
+        {
+            var result = await _serviceManager.BasketService.UpdateBasketAsync(basketDto);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket(string id)
+        {
+            await _serviceManager.BasketService.DeleteBasketAsync(id);
+            return NoContent();
+        }
+
+    }
+}
