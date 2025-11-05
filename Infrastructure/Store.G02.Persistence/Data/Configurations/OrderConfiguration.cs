ca553d1bd7b4cf5de55ddf6dfb32cd8f5aeb812a
Mai <Mai@DESKTOP-MILA5UN>
2025-11-05 10:45:10 +0400

    Session07-V03

---
+++ Infrastructure/Store.G02.Persistence/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.G02.Domain.Entities.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Persistence.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(O => O.ShippingAddress);
+
+            builder.HasOne(O => O.DeliveryMethod)
+                   .WithMany()
+                   .HasForeignKey(O => O.DeliveryMethodId);
+
+            builder.HasMany(O => O.Items)
+                   .WithOne()
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(O => O.SubtTotal).HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
