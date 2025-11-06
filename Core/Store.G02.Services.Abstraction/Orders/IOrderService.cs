9f98ac67e81792b672d0d46af467771e60d33d18
Mai <Mai@DESKTOP-MILA5UN>
2025-11-06 06:45:43 +0400

    IOrderService

---
+++ Core/Store.G02.Services.Abstraction/Orders/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using Store.G02.Shared.Dtos.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Services.Abstraction.Orders
+{
+    public interface IOrderService
+    {
+        Task<OrderResponse?> CreateOrderAsync(OrderRequest order, string userEmail);
+        Task<IEnumerable<DeliveryMethodResponse>> GetAllDeliveryMethodAsync();
+        Task<OrderResponse?> GetOrderByIdForSpecificUserAsync(Guid id, string userEmail);
+        Task<IEnumerable<OrderResponse>> GetOrdersByIdForSpecificUserAsync(string userEmail);
+    }
+}
---
+++ Store.G02.Shared/Dtos/Orders/OrderResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Shared.Dtos.Orders
+{
+    public class OrderResponse
+    {
+        public Guid Id { get; set; }
+        public string UserEmail { get; set; }
+        public DateTimeOffset OderDate { get; set; } = DateTimeOffset.Now;
+        public OrderAddressDto ShippingAddress { get; set; }
+        public string DeliveryMethod { get; set; }
+        public ICollection<OrderItemDto> Items { get; set; }
+        public decimal SubtTotal { get; set; }
+        public decimal MyProperty { get; set; }
+    }
+}
