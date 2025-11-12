62be0c97e0cb536f908f8e7e02fe274a24dabf44
Mai <Mai@DESKTOP-MILA5UN>
2025-11-12 09:13:33 +0400

    Add Webhook End Point

--- Infrastructure/Store.G02.Presentation/PaymentsController.cs
+++ Infrastructure/Store.G02.Presentation/PaymentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Store.G02.Services.Abstraction;
+using Stripe;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,48 @@ namespace Store.G02.Presentation
         {
             var result = await _serviceManager.PaymentService.CreatePaymentIntentAsync(basketId);
             return Ok(result);
+        }
+
+        // stripe listen --forward-to https://localhost:7155/api/payments/webhook
+        [Route("webhook")]
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            const string endpointSecret = "whsec_...";
+            try
+            {
+                var stripeEvent = EventUtility.ParseEvent(json);
+                var signatureHeader = Request.Headers["Stripe-Signature"];
+
+                stripeEvent = EventUtility.ConstructEvent(json,
+                        signatureHeader, endpointSecret);
+
+                // If on SDK version < 46, use class Events instead of EventTypes
+                if (stripeEvent.Type == EventTypes.PaymentIntentSucceeded)
+                {
+                    // Update Order Status To Success
+                }
+                else if (stripeEvent.Type == EventTypes.PaymentIntentPaymentFailed)
+                {
+                    // Update Order Status To Failed
+
+                }
+                else
+                {
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                return BadRequest();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
         }
     }
 }
